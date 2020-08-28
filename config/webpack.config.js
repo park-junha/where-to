9d991351b45a726dd736a346f8dc9a4ca1e1db6a
Junha Park <jpark3@scu.edu>
2020-08-27 20:39:42 -0700

    Suppress lgtm alert on env.stringified

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -555,7 +555,7 @@ module.exports = function(webpackEnv) {
       // It is absolutely essential that NODE_ENV is set to production
       // during a production build.
       // Otherwise React will be compiled in the very slow development mode.
-      new webpack.DefinePlugin(env.stringified),
+      new webpack.DefinePlugin(env.stringified), // lgtm [js/build-artifact-leak]
       // This is necessary to emit hot updates (currently CSS only):
       isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
       // Watcher doesn't work well if you mistype casing in a path so we use
