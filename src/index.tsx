3fe1820d660821bbe5020454963f328a5d4d819c
Junha Park <jpark3@scu.edu>
2020-09-16 19:06:53 -0700

    CSS -> Sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './styles.sass';
 import App from './App';
 
 ReactDOM.render(
