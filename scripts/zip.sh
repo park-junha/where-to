f8e8eb5efd9bc981336785e41d025fb18af4df7a
Junha Park <jpark3@scu.edu>
2021-11-01 13:54:06 -0700

    (1.5.4) Update styles and zip shell script

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
-BASE_DIR="$(cd "$(dirname "$0" )" && cd .. && pwd )"
+REPO_ROOT_DIR="$(cd "$(dirname "$0" )" && cd .. && pwd )"
+TARGET_DIR=$REPO_ROOT_DIR/artifacts
 SUCCESS='\033[1;36m'
 WARN='\033[1;33m'
 ERR='\033[1;31m'
 NC='\033[0m'
 
-cd $BASE_DIR/
-if [[ -f $BASE_DIR/build.zip ]]; then
-    echo -e "${WARN}WARN: build.zip exists, removing old copy${NC}"
-    rm $BASE_DIR/build.zip
-fi
-if [[ -f $BASE_DIR/src.zip ]]; then
-    echo -e "${WARN}WARN: src.zip exists, removing old copy${NC}"
-    rm $BASE_DIR/src.zip
+cd $REPO_ROOT_DIR/
+
+if [[ ! -d $TARGET_DIR ]]; then
+    echo -e "${WARN}WARN: $TARGET_DIR does not exist, creating directory${NC}"
+    mkdir $TARGET_DIR
 fi
 
-cd $BASE_DIR/build/ && zip -r ../build.zip *
+cd $TARGET_DIR/
+if [[ -f $TARGET_DIR/build.zip ]]; then
+    echo -e "${WARN}WARN: build.zip exists, removing old copy${NC}"
+    rm $TARGET_DIR/build.zip
+fi
+if [[ -f $TARGET_DIR/src.zip ]]; then
+    echo -e "${WARN}WARN: src.zip exists, removing old copy${NC}"
+    rm $TARGET_DIR/src.zip
+fi
+
+cd $REPO_ROOT_DIR/build/ && zip -r $TARGET_DIR/build.zip *
 RC_BUILD=$?
 if [[ $RC_BUILD -eq 0 ]]; then
-    echo -e "${SUCCESS}Successfully zipped build artifacts to $BASE_DIR/build.zip!${NC}"
+    echo -e "${SUCCESS}Successfully zipped build artifacts to $TARGET_DIR/build.zip!${NC}"
 else
     echo -e "${ERR}ERR: Could not zip build artifacts.${NC}"
     echo -e "Exit code: ${ERR}$RC_BUILD${NC}"
     exit $RC_BUILD
 fi
 
-cd $BASE_DIR && zip -r src.zip \
+cd $REPO_ROOT_DIR/ && zip -r $TARGET_DIR/src.zip \
     CONTRIBUTING.md \
     LICENSE.txt \
     README.md \
@@ -38,7 +46,7 @@ cd $BASE_DIR && zip -r src.zip \
     yarn.lock
 RC_SRC=$?
 if [[ $RC_SRC -eq 0 ]]; then
-    echo -e "${SUCCESS}Successfully zipped source code to $BASE_DIR/build.zip!${NC}"
+    echo -e "${SUCCESS}Successfully zipped source code to $TARGET_DIR/src.zip!${NC}"
 else
     echo -e "${ERR}ERR: Could not zip source code.${NC}"
     echo -e "Exit code: ${ERR}$RC_SRC${NC}"
